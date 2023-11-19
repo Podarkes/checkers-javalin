d60e68633f4ba05f11c6cd048fb414c41b9c90f3
Podarkes <yevgen.gryaznov@gmail.com>
2023-11-19 20:27:42 +0100

    Add h2 dependency

--- src/main/java/io/podarkes/Main.java
+++ src/main/java/io/podarkes/Main.java
@@ -5,18 +5,23 @@ import io.javalin.Javalin;
 import io.podarkes.controllers.GameController;
 import io.podarkes.controllers.MoveController;
 import io.podarkes.controllers.PlayerController;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        Server.createWebServer().start();
         Javalin app = Javalin.create().start(7000);
 
         app.get("/games", ctx -> ctx.json(GameController.getAllGames()));
         app.put("/games", ctx -> ctx.json(GameController.joinLobby(ctx.body())));
         app.post("/games", ctx -> ctx.json(GameController.startLobby(ctx.body())));
-        app.get("/games/<gameId>", ctx -> ctx.json(GameController.getGameById(ctx.pathParam("gameId"))));
+        app.get("/games/{gameId}", ctx -> ctx.json(GameController.getGameById(ctx.pathParam("gameId"))));
 
-        app.get("/games/<gameId>/moves", ctx -> ctx.json(MoveController.getMoves(ctx.body())));
-        app.put("/games/<gameId>/moves", ctx -> ctx.json(MoveController.makeMove(ctx.body())));
+        app.get("/games/{gameId}/moves", ctx -> ctx.json(MoveController.getMoves(ctx.body())));
+        app.put("/games/{gameId}/moves", ctx -> ctx.json(MoveController.makeMove(ctx.body())));
 
         app.get("/players", ctx -> ctx.json(PlayerController.getAllPlayers()));
         app.get("/players/{playerId}", ctx -> ctx.json(PlayerController.getPlayer(Long.valueOf(ctx.pathParam("playerId")))));
