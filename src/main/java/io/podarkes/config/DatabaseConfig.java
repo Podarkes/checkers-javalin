5e4d00f89757224343dd1623eb5034eda7ef9005
Podarkes <yevgen.gryaznov@gmail.com>
2023-11-20 15:53:59 +0100

    Add datasource

---
+++ src/main/java/io/podarkes/config/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package io.podarkes.config;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+    public static DataSource getDataSource() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl(System.getenv("url"));
+        dataSource.setUser(System.getenv("user"));
+        dataSource.setPassword(System.getenv("password"));
+        return dataSource;
+    }
+}
