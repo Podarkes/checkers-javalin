695af138ed8b075a34c9d6de30e1cceb66862ded
Podarkes <yevgen.gryaznov@gmail.com>
2023-11-20 18:18:04 +0100

    Configure Flyway

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into player (id, name) values (1, 'Alice');
-insert into player (id, name) values (2, 'Bob');
-
-insert into game (id, player_one_id, progress) values ('0a83d555-abfe-42f9-8d46-0d23d7a1d863', 1,'LOBBY');
-insert into game (id, player_one_id, progress) values ('1f3bea4e-48e3-4b7b-8ff8-6287f201b510', 2,'LOBBY');
---
+++ src/main/resources/db/migration/V1__Create_player_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE player (ID INT NOT NULL, NAME VARCHAR(100) NOT NULL);
+
+INSERT INTO player (id, name) VALUES (0, 'Alice');
+INSERT INTO player (id, name) VALUES (1, 'Bob');
